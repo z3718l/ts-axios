0d00b7a3540bf4aa46571e3dca10580b0e11b56a
Zhang Long <979463796@qq.com>
2019-08-07 09:56:21 +0800

    feat(配置文件，入口文件，样式文件): 第一个小demo完成
    第一个引入axios的demo完成，配合node webpack

--- examples/server.js
+++ examples/server.js
@@ -5,6 +5,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
 
+// express是一种中间件
 const app = express()
 const compiler = webpack(WebpackConfig)
 
@@ -16,4 +17,25 @@ app.use(webpackDevMiddleware(compiler, {
   }
 }))
 
+app.use(webpackHotMiddleware(compiler))
 
+// static静态资源目录为当前文件
+app.use(express.static(__dirname))
+
+// bodyParser：解析发送过来的body中的数据
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const router = express.Router()
+router.get('/simple/get', function (req, res) { 
+  res.json({
+    msg: 'Hello Word'
+  })
+ })
+
+ app.use(router)
+
+ const port = process.env.PORT || 3000
+ module.exports = app.listen(port, () => {
+  console.log('server is ok')
+})
---
+++ examples/simple/app.ts
@@ -0,0 +1,11 @@
+// 作为入口文件编译
+import axios from '../../src/index'
+
+axios({
+  method: 'get',
+  url: '/simple/get',
+  params: {
+    a: 1,
+    b: 2
+  }
+})

a54b26b73d85ca4e3905eb45d24333b82dd580c7
Zhang Long <979463796@qq.com>
2019-08-08 10:51:22 +0800

    feat: 处理返回数据
    如果不强制修改成json对象，我们在axios中也把string类型转换成json对象

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -7,3 +7,13 @@ export function transformRequest(data: any): any {
   }
   return data
 }
+
+// 处理data返回 string=》json对象
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {}
+  }
+  return data
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 // 定义一个axios方法
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 // 发送请求之前对config做处理
@@ -31,6 +33,11 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 // 导出
